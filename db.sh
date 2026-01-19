e1d36bbb2087cbcc7b3623d9c9a6569d19dbe03a
geno <alvinfairani@gmail.com>
2026-01-19 10:15:50 +0700

    Update db.sh

--- db.sh
+++ db.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
-# --- PRE-FIX: Menghapus karakter hantu Windows (\r) jika ada ---
-# Baris ini memastikan script berjalan lancar di Linux meskipun diedit di Windows
-sed -i 's/\r$//' "$0"
-
 # UPDATE & UPGRADE
 echo "Updating repositories..."
+sleep 1
 apt update -y && sudo apt upgrade -y
 
 # MARIADB INSTALLATION
 echo "Installing mariadb....."
+sleep 1
 # Menggunakan --fix-missing untuk mengatasi error 'Failed to fetch' dari mirror
 apt install -y --fix-missing mariadb-server mariadb-client
 apt --fix-broken install -y
@@ -20,8 +17,10 @@ echo "installing mariadb finish"
 DB_NAME="moodle"
 DB_USER="moodleuser"
 DB_PASS="12345"
-DB_HOST="%"
-
+# DB_HOST="%"
+echo -n "Masukkan ip web server: "
+read -s DB_HOST
+echo
 # Meminta input password root MySQL agar aman
 echo -n "Masukkan password root MySQL: "
 read -s ROOT_PASS
@@ -46,27 +45,6 @@ else
     echo "Terjadi kesalahan saat mengonfigurasi database."
 fi
 
-# REWRITE BINDING IP
-FILE_PATH="/etc/mysql/mariadb.conf.d/50-server.cnf"
-
-if [ -f "$FILE_PATH" ]; then
-    echo "Mengubah bind-address di $FILE_PATH..."
-    # Menggunakan regex yang lebih fleksibel untuk menangkap variasi spasi
-    sudo sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' "$FILE_PATH"
-
-    CHECK=$(grep "bind-address = 0.0.0.0" "$FILE_PATH")
-    
-    if [ ! -z "$CHECK" ]; then
-        echo "Berhasil: bind-address telah diubah menjadi 0.0.0.0"
-        echo "Me-restart MariaDB..."
-        sudo systemctl restart mariadb
-    else
-        echo "Gagal: Baris bind-address tidak ditemukan di file."
-    fi
-else
-    echo "Error: File $FILE_PATH tidak ditemukan."
-fi
-
 # FINISHING
 systemctl restart mariadb
 systemctl restart mysql 
@@ -88,6 +66,7 @@ echo "THANK YOU FOR USING ALVIN'S PRODUCT"
 # Membersihkan folder db/ (Pastikan script ini tidak sedang dijalankan di dalam folder tersebut)
 rm -rf db
 rm -rf "$0"
+
 
 
 
